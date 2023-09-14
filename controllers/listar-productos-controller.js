d22019ae376c8c5e386a9b14462866d59a247ff7
romina <rominaretamoso23@gmail.com>
2023-09-14 18:46:09 -0300

    arreglos de editar y eliminar

--- controllers/listar-productos-controller.js
+++ controllers/listar-productos-controller.js
@@ -3,16 +3,24 @@ import { productoServices } from "../services/producto-servicio.js";
 const nuevoProducto = (imagenUrl,name,precio,id) => {
     const card = document.createElement("div");
     const contenido = `
-                        <div class="card_producto_mostrado">                         
-                        <img  width="12px" height="12px" src="../img/edit.svg" alt="editar">
-                        <img  width="12px" height="12px" src="../img/eliminate.png" alt="editar">                          
+                        <div class="card_producto_mostrado"> 
+                        <div class="edit_eliminar">
+                        <a href=""><img  width="12px" height="12px" src="../img/edit.svg" alt="editar"></a>
+                        <a id="${id}" data-eliminar><img  width="12px" height="12px" src="../img/eliminate.png" alt="editar"></a>
+                        </div>                                                                    
                         <img src="${imagenUrl}" alt="imagen">
                         <h5 class="card_font">${name}</h5>
                         <p class="card_font">${precio}</p>             
                       
                         </div>    
                        `;
-    card.innerHTML = contenido;    
+    card.innerHTML = contenido;
+    const eliminar = card.querySelector("[data-eliminar]");
+    
+    eliminar.addEventListener("click", () =>{
+      const id = eliminar.id;
+      productoServices.eliminarProducto(id);
+    });    
     return card;                  
 };
 
